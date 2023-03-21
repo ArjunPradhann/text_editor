2b3f36f464346f0e1b102bbdf34fba743708a0ba
Arjun Pradhan <arjunpradhan46@gmail.com>
2023-03-21 13:20:01 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline text-blue-900">
-      Arjun Pradhan
-    </h1>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
   );
 }
 
